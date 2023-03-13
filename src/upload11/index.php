a5f68817a1fd9664a11c5c018343c526ffcc7116
faizala-synopsys <127382244+faizala-synopsys@users.noreply.github.com>
2023-03-13 16:22:27 +0530

    Update index.php

--- src/upload11/index.php
+++ src/upload11/index.php
@@ -28,7 +28,7 @@
     </div>
 
     <?php
-    $target_dir = "uploads/";
+    $target_dir = "upload11/";
     
     if(isset($_POST["submit"])) {
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
